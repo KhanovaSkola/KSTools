a1a3a6ddb2f72b6c11ea879c79ccd9fa17189e43
Daniel Hollas <danekhollas@gmail.com>
2019-01-24 19:16:11 +0100

    Add printing for Bakalari e-learning system

--- print_ka_json.py
+++ print_ka_json.py
@@ -7,16 +7,16 @@ import json
 
 def read_cmd():
    """Reading command line options."""
-   desc = "Program for downloading and printing Khan Academy content tree."
+   desc = "Program for linking Khan content for PPUC reputation system."
    parser = argparse.ArgumentParser(description=desc)
-   parser.add_argument('-d','--download',dest='download',default=False, action='store_true', help='Download most up-to-date full tree?')
    parser.add_argument('-s','--subject', dest='subject', default='root', help='Print full tree for a given domain/subject.')
    parser.add_argument('-c','--content', dest='content', default='', help='Which kind of content should we download? Options: video|exercise|article|topic')
-   parser.add_argument('-l','--list', dest='list', default=False,action='store_true', help='Only list topic names within given domain/subject/topic.')
+   parser.add_argument('-l','--list', dest='list', default=False, action='store_true', help='Only list topic names within given domain/subject/topic.')
+   parser.add_argument('-b','--bakalari', dest='bakalari', default=False, action='store_true', help='Print info for Bakalari linking.')
    return parser.parse_args()
 
 # Currently, article type does not seem to work.
-content_types = ['video', 'article', 'exercise', 'topic']
+CONTENT_TYPES = ['video', 'article', 'exercise', 'topic']
 LISTED_CONTENT_FILE = 'indexable_slugs.txt'
 
 
@@ -46,31 +46,27 @@ def read_listed_content_slugs(listed_content_file):
 if __name__ == '__main__':
 
     opts = read_cmd()
-    download = opts.download
     topic_title  = opts.subject
     content_type = opts.content.lower()
     lst = opts.list
 
-    if content_type not in content_types:
+    if content_type not in CONTENT_TYPES:
         print("ERROR: content argument: ", opts.content)
-        print("Possibilities are: ", content_types)
+        print("Possibilities are: ", CONTENT_TYPES)
         exit(1)
 
-    if download:
-        tree = kapi_download_topictree(content_type)
-        if tree != None:
-            #save_obj_text(tree, 'KAtree_'+what+'_txt')
-            save_obj_bin(tree, 'KAtree_' + content_type + '_bin')
-        else:
-            tree = load_ka_tree(content_type)
-    else:
-        #tree = load_obj_bin('KAtree_'+what+'_bin')
-        tree = load_ka_tree(content_type)
+    khan_tree = KhanContentTree('cs', content_type)
+    tree = khan_tree.get()
 
     # Pick just concrete topic from KA tree
-    subtree = find_ka_topic(tree, topic_title)
+    if topic_title != 'root':
+        subtree = find_ka_topic(tree, topic_title)
+    else:
+        subtree = tree
+
     if not subtree:
         print("ERROR: Could not find subtree for course: %s\n" % (topic_title))
+        print(subtree)
         # DH hack
         #subtree = tree
         sys.exit(1)
@@ -85,6 +81,86 @@ if __name__ == '__main__':
     # KA API returns unlisted content as well, need to deal with that externally
     listed_content = read_listed_content_slugs(LISTED_CONTENT_FILE)
 
+    # Printing for Bakalari`
+    if opts.bakalari:
+        ksid_ytid_map_file = 'ka_indexable_ytid_ksid_map.dat'
+        unique_content_ids = set()
+        ytid_ksid_map = {}
+        ytid_on_ks = set()
+        with open(ksid_ytid_map_file, 'r') as f:
+            for line in f:
+                l = line.split()
+                if len(l) < 2 and len(l) > 3:
+                    print("Fatal error in reading file;", ksid_ytid_map_file)
+                ksid = l[0]
+                ytid = l[1]
+                ytid_ksid_map[ytid] = ksid
+                if len(l) == 3:
+                    ytid_original = l[2]
+                    ytid_ksid_map[ytid_original] = ksid
+                ytid_on_ks.add(ytid)
+
+
+        ka = KhanAPI('cs')
+        with open('bakalari_relinking_%s_%s.csv' % (topic_title.replace(' ', '_').lower(), content_type), 'w', encoding = 'utf-8') as out:
+            for c in content:
+                # Take care of duplicates (we're assuming that we're looking at the whole Math at once
+                if c['id'] not in unique_content_ids:
+                    unique_content_ids.add(c['id'])
+                else:
+                    continue
+
+                # Bakalari fields
+                dodavatel = 'Khan Academy'
+                culture = '--'
+                lang = {
+                    'audio': 'CZ',
+                    'subtitles': '--',
+                    'text': 'CZ'
+                }
+
+                ytid = c['youtube_id']
+                translated_ytid = c['translated_youtube_id']
+                vid = c
+                # Here we workaround a bug in API which sometimes seems to return wrong youtube_id
+                if c['youtube_id'] == c['translated_youtube_id']:
+                    vid = ka.download_video(c['readable_id'])
+                    if vid:
+                        ytid = vid['youtube_id']
+                    else:
+                        print(c)
+                        print("ERROR: Could not find video, ytid = ", ytid)
+                        sys.exit(1)
+                    #print(c)
+
+                # Subtitled video, modify fields accordingly
+                # Unfortunatelly, we cannot properly detect dubbed videos for now...
+                if vid['youtube_id'] == vid['translated_youtube_id']:
+                    lang = {
+                        'audio': 'EN',
+                        'subtitles': 'CS',
+                        'text': 'EN'
+                    }
+
+                # Strip newlines (not tested) and occacional semicolons
+                vid['translated_title'].replace('\n','').replace(';',',')
+                vid['translated_description'].replace('\n','').replace(';',',')
+
+                if ytid in ytid_ksid_map.keys():
+                    out.write('%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n' % (ytid_ksid_map[ytid],
+                        vid['id'], vid['translated_title'], vid['translated_description'], vid['ka_url'],
+                        dodavatel, lang['audio'], lang['subtitles'], lang['text'], culture))
+                elif translated_ytid in ytid_ksid_map.keys():
+                    out.write('%s;%s;%s;%s;%s;%s;%s;%s;%s;%s\n' % (ytid_ksid_map[translated_ytid],
+                        vid['id'], vid['translated_title'], vid['translated_description'], vid['ka_url'],
+                        dodavatel, lang['audio'], lang['subtitles'], lang['text'], culture))
+
+        print("Number of retrieved content items = ", len(unique_content_ids))
+        print("Number of videos on Khanova skola = ", len(ytid_on_ks))
+        sys.exit(0)
+
+
+    # Printing for PPUC reputation site
     courses = {
         'Early math': 'early',
         'Arithmetic': 'arith',
@@ -238,10 +314,6 @@ if __name__ == '__main__':
     print("Number of PPUC content items = %d" % len(ppuc_content))
 #    print("Number of unique ids = %d" % (len(unique_content_ids)))
 
-    # Printing for Bakalari`
-#    with open('ka_%s_%s.csv' % (topic_title.replace(' ', '_').lower(), content_type), 'w', encoding = 'utf-8') as out:
-#        for c in content:
-#            out.write('%s;%s;%s;%s\n' % (c['id'], c['youtube_id'], c['translated_youtube_id'],c['ka_url']))
 
-#    pprint(ppuc_content[-1])
+#    pprint(content[-1])
 
