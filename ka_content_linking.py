f43cfacb4bea4ffd205bf00f7388339282b73f5a
Daniel Hollas <danekhollas@gmail.com>
2019-03-28 15:53:29 +0100

    EMA: Refactor - use KA slugs and EMA attributes

--- ka_content_linking.py
+++ ka_content_linking.py
@@ -9,15 +9,58 @@ def read_cmd():
    """Reading command line options."""
    desc = "Program for linking CS-Khan content for EMA reputation system."
    parser = argparse.ArgumentParser(description=desc)
-   parser.add_argument('-s','--subject', dest='subject', default='root', help='Link given domain/subject.')
-   parser.add_argument('-c','--content', dest='content', default='', help='Content kind: video|exercise|article|topic')
+   parser.add_argument('-s','--subject', dest='subject', default='root', help='Link given course.')
+   parser.add_argument('-c','--content', dest='content', default='', help='Content kind: video|exercise')
+   # TODO: Add verbose parameter
    return parser.parse_args()
 
 # Currently, article type does not seem to work.
-CONTENT_TYPES = ['video', 'article', 'exercise', 'topic']
+CONTENT_TYPES = ['video', 'exercise']
 LISTED_CONTENT_FILE = 'indexable_slugs.txt'
 #TODO
 SKIPPED_CONTENT_FILE = 'skipped_slugs.txt'
+
+EMA_OPTIONAL_DATA = {
+    # TODO: What about subtitles videos?
+    'jazyk': '5-cs',
+    'autor': 'Khan Academy',
+    'dostupnost': '7-ANO', # OER
+    'typ': {
+        'video': '8-VI',
+        'exercise': '8-IC',
+        'article': '8-CL'
+    },
+    'licence': {
+        'cc-by-nc-nd': '1-CCBYNCND30',
+        'cc-by-nc-sa': '1-CCBYNCSA30',
+        'cc-by-sa': '1-CCBYSA30',
+        'yt-standard': '1-OST'
+    },
+    'stupen_vzdelavani': {
+        'early-math': '2-Z',
+        'arithmetic': '2-Z',
+        'pre-algebra': '2-Z',
+        'basic-geo': '2-Z',
+        'algebra-basics': '2-Z',
+        'trigonometry': '2-G',
+        'music': '2-NU',
+        'cosmology-and-astronomy': '2-NU'
+    },
+    'vzdelavaci_obor': {
+        'math': '9-03',
+        'music': '9-11',
+        'astro': '9-09' # TODO: 9-10' # Not clear whether we can have multiple types
+    },
+    'rocnik': {
+        'early-math': '3-Z13',
+        'trigonometry': '3-SS',
+    },
+    'gramotnost': {
+        'math': '4-MA',
+        'music': '4-NU',
+        'astro': '4-PR'
+    }
+}
 
 # TODO: Rename this function, re-use for skipped content
 def read_listed_content_slugs(listed_content_file):
@@ -36,7 +79,7 @@ def read_listed_content_slugs(listed_content_file):
 if __name__ == '__main__':
 
     opts = read_cmd()
-    topic_title  = opts.subject
+    course = opts.subject
     content_type = opts.content.lower()
 
     if content_type not in CONTENT_TYPES:
@@ -44,73 +87,53 @@ if __name__ == '__main__':
         print("Possibilities are: ", CONTENT_TYPES)
         exit(1)
 
+    # Handle duplicities across courses
+    # The ordering here is important!
+    math_courses = ['early-math', 'arithmetic', 'basic-geo', 'trigonometry', 'algebra-basics', 'pre-algebra']
+    available_courses = math_courses
+    available_courses.append('music')
+    available_courses.append('cosmology-and-astronomy')
+
+    if course not in available_courses:
+        eprint('ERROR: Invalid course! Valid course are:')
+        eprint(courses)
+        sys.exit(1)
+
     khan_tree = KhanContentTree('cs', content_type)
     tree = khan_tree.get()
 
-    # Pick just concrete topic from KA tree
-    if topic_title != 'root':
-        subtree = find_ka_topic(tree, topic_title)
-    else:
-        subtree = tree
+    subtree = find_ka_topic(tree, course)
 
     if not subtree:
-        print("ERROR: Could not find subtree for course: %s\n" % (topic_title))
+        print("ERROR: Could not find subtree for course: %s\n" % (course))
         sys.exit(1)
 
     content = []
-    ppuc_content = []
+    ema_content = []
     unique_content_ids = set()
-    # TODO: Filter out only given content type
+
     kapi_tree_get_content_items(subtree, content, content_type)
 #    pprint(content[0].keys())
         
     # KA API returns unlisted content as well, need to deal with that externally
     listed_content = read_listed_content_slugs(LISTED_CONTENT_FILE)
 
-    # TODO: Get rid of this and just use slugs everywhere
-    #math_courses = ('early-math', 'arithmetic', 'basic-geo', 'trigonometry', 'algebra-basics', 'pre-algebra')
-    #courses = math_courses
-    #courses.append('music')
-    #courses.append('mology-and-astronomy')
-    #course = topic_title
-
-    courses = {
-        'early-math': 'early',
-        'arithmetic': 'arith',
-        'trigonometry': 'trig',
-        'basic-geo': 'basicgeo',
-        'pre-algebra': 'prealg',
-        'algebra-basics': 'basicalg',
-        'music': 'music',
-        'cosmology-and-astronomy': 'astro'
-    }
-
     course_subject_map = {
         'music': 'music',
-        'astro': 'astro',
-        'early': 'math',
-        'arith': 'math',
-        'basicgeo': 'math',
-        'basicalg': 'math',
-        'prealg': 'math',
-        'trig': 'math'
+        'cosmology-and-astronomy': 'astro',
+        'early-math': 'math',
+        'arithmetic': 'math',
+        'basic-geo': 'math',
+        'algebra-basics': 'math',
+        'pre-algebra': 'math',
+        'trigonometry': 'math'
     }
-
-    try:
-        course = courses[topic_title]
-    except:
-        eprint('ERROR: Invalid course! Valid course are:')
-        eprint(courses)
-        sys.exit(1)
 
     subject = course_subject_map[course]
 
-    # Handle duplicities across courses
-    # The ordering here is important!
-    math_courses = ('early-math', 'arithmetic', 'basic-geo', 'trigonometry', 'algebra-basics', 'pre-algebra')
     # Go through all "preceding" math courses
     for crs in math_courses:
-        if crs == topic_title:
+        if crs == course:
             break
         subtree = find_ka_topic(tree, crs)
         temp_content = []
@@ -120,54 +143,6 @@ if __name__ == '__main__':
 
     if subject == 'math':
         print("Number of unique ids from preceding math courses = %d" % (len(unique_content_ids)))
-
-    # TODO: Make ppuc_data keys same with json keys
-    ppuc_data = {
-        'lang': '5-cs',
-         'source_type': '7-ANO', # OER
-         'ct_types': {
-            'video': '8-VI',
-            'exercise': '8-IC',
-            'article': '8-CL'
-        },
-        'licenses': {
-            'cc-by-nc-nd': '1-CCBYNCND30',
-            'cc-by-nc-sa': '1-CCBYNCSA30',
-            'cc-by-sa': '1-CCBYSA30',
-            'yt-standard': '1-OST'
-        },
-        'stupen': {
-            'early': '2-Z',
-            'arith': '2-Z',
-            'prealg': '2-Z',
-            'basicgeo': '2-Z',
-            'basicalg': '2-Z',
-            'trig': '2-G',
-            'music': '2-NU',
-            'astro': '2-NU'
-        },
-        'subjects': {
-            'math': '9-03',
-            'music': '9-11',
-            'astro': '9-09' # TODO: 9-10' # Not clear whetherwe can have multiple types
-        },
-        'grades': {
-            'early': '3-Z13',
-            'arith': '',
-            'prealg': '',
-            'basicgeo': '',
-            'basicalg': '',
-            'trig': '3-SS',
-            'music': '3-NU',
-            'astro': '3-NU'
-        },
-        'gramotnost': {
-            'math': '4-MA',
-            'music': '4-NU',
-            'astro': '4-PR'
-        }
-    }
-    
 
     for v in content:
 
@@ -189,29 +164,31 @@ if __name__ == '__main__':
             'nazev': v['translated_title'],
             'popis': v['translated_description'],
             # Fixed items
-            'autor': 'Khan Academy',
-            'jazyk': ppuc_data['lang'],
-            'dostupnost': ppuc_data['source_type'],
+            'autor': EMA_OPTIONAL_DATA['autor'],
+            'jazyk': EMA_OPTIONAL_DATA['jazyk'],
+            'dostupnost': EMA_OPTIONAL_DATA['dostupnost'],
             # Optional fields
+            'typ': EMA_OPTIONAL_DATA['typ'][content_type],
+            'stupen_vzdelavani': EMA_OPTIONAL_DATA['stupen_vzdelavani'][course],
+            'vzdelavaci_obor': EMA_OPTIONAL_DATA['vzdelavaci_obor'][subject],
+            'gramotnost': EMA_OPTIONAL_DATA['gramotnost'][subject],
             # TODO: Creation date might not make sense...
-            'datum_vzniku' : v['creation_date'],
-            'vzdelavaci_obor': ppuc_data['subjects'][subject],
-            'typ': ppuc_data['ct_types'][content_type],
-            'stupen_vzdelavani': ppuc_data['stupen'][course],
-            'gramotnost': ppuc_data['gramotnost'][subject]
+            'datum_vzniku': v['creation_date']
             # KA API gives keywords in EN, commenting out for now....
 #            'klicova_slova': v['keywords'],
 #            'otevreny_zdroj': '7-ANO',
           }
 
-          # Do not export empty fieds...TODO: should make this more general
-          if course in ppuc_data['grades'].keys() and ppuc_data['grades'][course] != '':
-              item['rocnik'] = ppuc_data['grades'][course]
+          # Do not export empty fieds...
+          # TODO: should make this more general
+          if course in EMA_OPTIONAL_DATA['rocnik'].keys() and EMA_OPTIONAL_DATA['rocnik'][course] != '':
+              item['rocnik'] = EMA_OPTIONAL_DATA['rocnik'][course]
 
+          # TODO: Refactor this, it is confusing
           if 'ka_user_licence' in v.keys():
-             item['licence'] = ppuc_data['licenses'][v['ka_user_license']]
+             item['licence'] = EMA_OPTIONAL_DATA['licence'][v['ka_user_license']]
           else:
-             item['licence'] = ppuc_data['licenses']['cc-by-nc-sa'] # Let's just take the KA default
+             item['licence'] = EMA_OPTIONAL_DATA['licence']['cc-by-nc-sa'] # Let's just take the KA default
 
           if item['licence'] == '1-OST':
               if v['ka_user_licence'] == 'yt-standard':
@@ -220,15 +197,15 @@ if __name__ == '__main__':
                 eprint("WARNING: Missing license URL!")
                 del item['licence']
 
-          ppuc_content.append(item)
+          ema_content.append(item)
 
         except:
             eprint('Key error!')
             eprint(v)
             raise
  
-    with open('ka_%s_%s.json' % (topic_title.replace(' ', '_').replace('-', '_').lower(), content_type), 'w', encoding = 'utf-8') as out:
-        out.write(json.dumps(ppuc_content, ensure_ascii=False))
+    with open('ka_%s_%s.json' % (course.replace('-', '_').lower(), content_type), 'w', encoding = 'utf-8') as out:
+        out.write(json.dumps(ema_content, ensure_ascii=False))
 
-    print("Number of EMA content items in %s = %d" % (course, len(ppuc_content)))
+    print("Number of EMA content items in %s = %d" % (course, len(ema_content)))
 
