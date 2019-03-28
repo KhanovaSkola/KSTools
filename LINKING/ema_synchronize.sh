360f849426eab97abf67fd8b7dabc7a8f08b3415
Daniel Hollas <danekhollas@gmail.com>
2019-03-28 15:20:01 +0100

    Minor tweaks to EMA synchro script

--- LINKING/ema_synchronize.sh
+++ LINKING/ema_synchronize.sh
@@ -1,26 +1,57 @@
 #!/bin/bash
 
-# TODO: Download latest CS-KA tree for exercises and videos
+# Only for debuggind
+function compare_reference {
+  course=$1
+  ct_type=$2
+  fname=`echo $course | sed 's/-/_/g'`
+  fname=ka_${fname}_${ct_type}.json
+  diff -q $fname ref/$fname
+  if [[ $? -ne 0 ]];then
+    exit 1
+  fi
+}
+
+SYNC=true
+DOWNLOAD=true
+
+if [[ $DOWNLOAD = 'true' ]];then
+  ../download_KAtree.py -l cs -c video
+  if [[ $? -ne 0 ]];then
+    echo "ERROR: Could not get exercises for course $course"
+    exit 1
+  fi
+  ../download_KAtree.py -l cs -c exercise
+  if [[ $? -ne 0 ]];then
+    echo "ERROR: Could not get exercises for course $course"
+    exit 1
+  fi
+fi
 
 courses=('cosmology-and-astronomy' 'early-math' 'arithmetic' \
   'trigonometry' 'basic-geo' 'pre-algebra' 'algebra-basics' 'music')
 
-
 # TODO: Use these iterations in Python, will be much faster, can load tree only once per content type
 # Need to refactor the python script first
-for s in ${courses[@]}
+for course in ${courses[@]}
 do
-  ../ka_content_linking.py -s $s -c video
+  ../ka_content_linking.py -s $course -c video
   if [[ $? -ne 0 ]];then
-    echo "ERROR: Could not get videos for subject $s"
+    echo "ERROR: Could not get videos for course $course"
     exit 1
   fi
-  ../ka_content_linking.py -s $s -c exercise
+#  compare_reference $course "video"
+  ../ka_content_linking.py -s $course -c exercise
   if [[ $? -ne 0 ]];then
-    echo "ERROR: Could not get exercises for subject $s"
+    echo "ERROR: Could not get exercises for course $course"
     exit 1
   fi
+#  compare_reference $course "exercise"
 done
+
+if [[ $SYNC != "true" ]];then
+  exit 0
+fi
 
 # Copy all json files to the server
 SRV="/srv/khanovaskola.cz/production/www/ppuc/"
